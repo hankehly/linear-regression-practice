c07a391f5d40354d2d3a46945ce21b92d76eb3c0
hank-ehly <hank.ehly@smapenergy.com>
2018-08-28 20:06:23 +0900

    Use dot product

--- src/functions.py
+++ src/functions.py
@@ -21,7 +21,8 @@ def read_dataset_2():
 
 def compute_cost(X, y, theta):
     m = len(y)
-    h = np.transpose(theta) * X
+    # h = np.transpose(theta) * X
+    h = X.dot(theta)
     error_sqr = (h - y) ** 2
     return np.sum(error_sqr, axis=0) / (2 * m)
 
@@ -30,11 +31,10 @@ def gradient_descent(X, y, theta, alpha, num_iters):
     m = len(y)
     j_history = np.zeros((num_iters, 1))
     for i in range(num_iters):
-        h = np.transpose(theta) * X
+        # h = np.transpose(theta) * X
+        h = X.dot(theta)
         errors = (h - y)
         gradient = (alpha / m) * np.sum(errors * X)
-        # make gradient broadcast-able to theta
-        gradient = np.reshape(gradient, (1,))
         theta = theta - gradient
-        j_history[i] = compute_cost(X, y, theta)  # [0]?
+        j_history[i] = compute_cost(X, y, theta)
     return theta, j_history
