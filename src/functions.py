08d47d4df509c389ca7550b142d9dfd759ea01b2
Hank Ehly <henry.ehly@gmail.com>
2018-08-28 09:24:39 +0900

    Add second dataset

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,22 @@
+import os
+
+import pandas as pd
+
 import numpy as np
+
+data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data')
+
+
+def read_dataset_2():
+    df = pd.read_csv(os.path.join(data_dir, 'dataset_2/ex1data1.txt'), names=['X', 'y'], header=None)
+    X = np.vstack(df['X'])
+    X_0 = np.ones(np.shape(X))
+    X = np.hstack((X_0, X))
+    y = np.vstack(df['y'])
+    theta = np.zeros((2, 1))
+    alpha = 0.01
+    num_iters = 1500
+    return X, y, theta, alpha, num_iters
 
 
 def compute_cost(X, y, theta):
@@ -18,5 +36,5 @@ def gradient_descent(X, y, theta, alpha, num_iters):
         # make gradient broadcast-able to theta
         gradient = np.reshape(gradient, (1,))
         theta = theta - gradient
-        j_history[i] = compute_cost(X, y, theta) # [0]?
+        j_history[i] = compute_cost(X, y, theta)  # [0]?
     return theta, j_history
