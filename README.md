4b42e5131468518032c11416f9a0b32f78f51e84
Hank Ehly <henry.ehly@gmail.com>
2018-08-29 09:43:00 +0900

    Add question to README.md

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Questions:
 - difference between element-wise vector/matrix multiplication and dot product
+- when computing the gradient, why do we not need to use np.sum?
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from src.functions import read_dataset_2, compute_cost, gradient_descent
 
 if __name__ == '__main__':
     X, y, theta, alpha, num_iters = read_dataset_2()
     new_theta, j_history = gradient_descent(X, y, theta, alpha, num_iters)
-    print(new_theta)
+    plt.plot(j_history)
+    plt.show()
