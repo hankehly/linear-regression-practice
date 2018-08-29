5f54bcd0530e2c550b72cb28a01dc794ffe0b0ec
Hank Ehly <henry.ehly@gmail.com>
2018-08-29 09:34:46 +0900

    Dynamically calculate the number of columns

--- src/functions.py
+++ src/functions.py
@@ -10,10 +10,11 @@ data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data')
 def read_dataset_2():
     df = pd.read_csv(os.path.join(data_dir, 'dataset_2/ex1data1.txt'), names=['X', 'y'], header=None)
     X = np.vstack(df['X'])
-    X_0 = np.ones(np.shape(X))
+    X_0 = np.ones(X.shape)
     X = np.hstack((X_0, X))
     y = np.vstack(df['y'])
-    theta = np.zeros((2, 1))
+    n = X.shape[1]
+    theta = np.zeros((n, 1))
     alpha = 0.01
     num_iters = 1500
     return X, y, theta, alpha, num_iters
