8ae666d92badf8787126fd2324febbc19194fccf
Hank Ehly <henry.ehly@gmail.com>
2018-08-31 08:29:26 +0900

    Add subplots

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
 
-from src.functions import read_dataset_2, gradient_descent, predict
+from src.functions import read_dataset_2, read_dataset_1, gradient_descent, predict
 
 if __name__ == '__main__':
     X, y, theta, alpha, num_iters = read_dataset_2()
     theta, j_history = gradient_descent(X, y, theta, alpha, num_iters)
 
+    # Plot cost function result history
+    plt.subplot(1, 2, 1)
+    plt.plot(j_history)
+    plt.title('learning rate = %2f' % alpha)
+    plt.xlabel('Number of iterations')
+    plt.ylabel('Cost J')
+
     # Plot regression line alongside scatter plot of data points
-    # plt.scatter(X[:, 1], y, c='#FF0000', marker='x')
-    # plt.plot(X[:, 1], X.dot(theta)[:, 0])
-    # plt.show()
+    plt.subplot(1, 2, 2)
+    plt.scatter(X[:, 1], y, c='#FF0000', marker='x')
+    plt.plot(X[:, 1], X.dot(theta)[:, 0])
+
+    plt.show()
