2ea028f284f9727b2672c489ca7e4e5c40bd21a6
Hank Ehly <henry.ehly@gmail.com>
2018-08-31 08:31:08 +0900

    Add functions

--- src/functions.py
+++ src/functions.py
@@ -20,6 +20,19 @@ def read_dataset_2():
     return X, y, theta, alpha, num_iters
 
 
+def read_dataset_1():
+    df = pd.read_csv(os.path.join(data_dir, 'dataset_1/train.csv'))
+    X = np.vstack(df['x']) / 10
+    X_0 = np.ones(X.shape)
+    X = np.hstack((X_0, X))
+    y = np.vstack(df['y']) / 10
+    n = X.shape[1]
+    theta = np.zeros((n, 1))
+    alpha = 0.03
+    num_iters = 1500
+    return X, y, theta, alpha, num_iters
+
+
 def compute_cost(X, y, theta):
     m = len(y)
     h = X.dot(theta)
@@ -33,7 +46,7 @@ def gradient_descent(X, y, theta, alpha, num_iters):
     for i in range(num_iters):
         h = X.dot(theta)
         errors = h - y
-        gradient = (alpha / m) * np.transpose(X).dot(errors)
+        gradient = (alpha / m) * X.T.dot(errors)
         theta = theta - gradient
         j_history[i] = compute_cost(X, y, theta)
     return theta, j_history
@@ -41,3 +54,16 @@ def gradient_descent(X, y, theta, alpha, num_iters):
 
 def predict(val, theta):
     return np.array([1, val]).dot(theta)
+
+
+def measure_dataset_1_model_accuracy():
+    X, y, theta, alpha, num_iters = read_dataset_1()
+    theta, _ = gradient_descent(X, y, theta, alpha, num_iters)
+
+    df = pd.read_csv(os.path.join(data_dir, 'dataset_1/test.csv'))
+    m = len(df)
+    for i in range(m):
+        row = df.iloc[i]
+        prediction = predict(row['x'], theta)
+        diff = np.abs(prediction - row['y'])
+        print('prediction: %s, actual: %s (diff: %s)' % (prediction, row['y'], diff))
